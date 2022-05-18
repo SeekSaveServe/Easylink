1e1bc9eb579a9aad191ab5dc5a5e0e61a83c91ad
Huzaifa Raghav <windowpane1712@gmail.com>
2022-05-18 19:04:03 +0800

    Add Hello World

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1> Hello World! </h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
