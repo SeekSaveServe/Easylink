234f0797aa2ee74c466ed59554eaf173ebec7206
Pei Cheng Yi <53963433+SeekSaveServe@users.noreply.github.com>
2022-05-19 17:04:55 +0800

    done with skeleton sign up page

--- src/sign up/Form.js
+++ src/sign up/Form.js
@@ -1,99 +1,129 @@
-import {useState} from 'react';
-import logo from './../Assets/Easylink Logo Full.png'
+import { useState } from "react";
 
-export default function Form() {
-// States for registration
-const [userName, setUserName] = useState("");
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-const [confirmPassword, setConfirmPassword] = useState('');
+export function Form() {
+  // States for registration
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-// States for checking errors
-const [submitted, setSubmitted] = useState(false);
-const [error, setError] = useState(false);
+  // States for checking errors
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
 
-// Handling name change
-const handleNameChange = (e) => {
+  // Handling name change
+  const handleNameChange = (e) => {
     setUserName(e.target.value);
     setSubmitted(false);
-};
+  };
 
-// Handling email change
-const handleEmailChange = (e) => {
+  // Handling email change
+  const handleEmailChange = (e) => {
     setEmail(e.target.value);
     setSubmitted(false);
-};
+  };
 
-// Handling password change
-const handlePasswordChange = (e) => {
+  // Handling password change
+  const handlePasswordChange = (e) => {
     setPassword(e.target.value);
     setSubmitted(false);
-};
+  };
 
-// Handling confirm password change
-const handleConfirmPasswordChage = (e) => {
+  // Handling confirm password change
+  const handleConfirmPasswordChage = (e) => {
     setConfirmPassword(e.target.value);
     setSubmitted(false);
-};
+  };
 
-// Handling the form submission
-const handleSubmit = (e) => {
+  // Handling the form submission
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (userName === '' || email === '' || password === '' || confirmPassword === '' || !(confirmPassword === password)) {
-        setError(true);
+    if (
+      userName === "" ||
+      email === "" ||
+      password === "" ||
+      confirmPassword === "" ||
+      !(confirmPassword === password)
+    ) {
+      setError(true);
     } else {
-        setSubmitted(true);
-        setError(false);
+      setSubmitted(true);
+      setError(false);
     }
-};
+  };
 
-// Show success message
-const successMessage = () => {
+  // Show success message
+  const successMessage = () => {
     return (
-        <div
-        className='successMessage'>
-        <h1> This is a placeholder message informing you that you have succeeded</h1>
-        </div>
+      <div
+        className="successMessage"
+        style={{
+          display: submitted ? "" : "none",
+        }}
+      >
+        <h1> Hi {userName}, </h1>
+      </div>
     );
-};
+  };
 
-
-// Show error message
-const errorMessage = () => {
+  // Show error message
+  const errorMessage = () => {
     return (
-    <div
-    className='errorMessage'
-    style={{
-        display: error? '' : 'none',
-    }}>
+      <div
+        className="errorMessage"
+        style={{
+          display: error ? "" : "none",
+        }}
+      >
         <h1> Please enter all fields</h1>
-    </div>      
+      </div>
     );
-};
+  };
 
-return (
+  return (
     <div className="form">
-        <div className="messages">
-            {errorMessage()}
-            {successMessage()}
-        </div>
-        <form> 
-        <label className='label'>Username</label>
-        <input 
-        onChange={handleName} 
-        className="input"
-        value={userName}
-        type="text"
+      <div className="messages">
+        {errorMessage()}
+        {successMessage()}
+      </div>
+      <form>
+        <label className="label">Username</label>
+        <input
+          onChange={handleNameChange}
+          className="input"
+          value={userName}
+          type="text"
         />
 
-        <label className='label'>Username</label>
-        <input 
-        onChange={handleName} 
-        className="input"
-        value={userName}
-        type="text"
-        />        
-        </form>
+        <label className="label">Email</label>
+        <input
+          onChange={handleEmailChange}
+          className="input"
+          value={email}
+          type="email"
+        />
+
+        <label className="label">Password</label>
+        <input
+          onChange={handlePasswordChange}
+          className="input"
+          value={password}
+          type="password"
+        />
+
+        <label className="label">Confirm Password</label>
+        <input
+          onChange={handleConfirmPasswordChage}
+          className="input"
+          value={confirmPassword}
+          type="password"
+        />
+
+        <button onClick={handleSubmit} className="button" type="submit">
+          {" "}
+          Create Account{" "}
+        </button>
+      </form>
     </div>
-)
+  );
 }
