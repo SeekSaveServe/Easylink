5a6a97270f2def786a8e671bbf490d873482426f
Huzaifa Raghav <windowpane1712@gmail.com>
2022-05-19 15:13:12 +0800

    Setup and install ChakraUI

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ChakraProvider resetCSS={false}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
