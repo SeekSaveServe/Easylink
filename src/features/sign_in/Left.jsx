3ebc599e54ed0d498d063ec215cd8561a80de982
Pei Cheng Yi <53963433+SeekSaveServe@users.noreply.github.com>
2022-05-20 16:22:11 +0800

    Added sign in with email and password

--- src/features/sign_in/Left.jsx
+++ src/features/sign_in/Left.jsx
@@ -1,34 +1,65 @@
-import { React } from 'react';
+import { useState } from "react";
+import { React } from "react";
+import { supabase } from "../../supabaseClient";
 
 function Left({ logo, msg, formState, updateState, handleSignIn }) {
-    return (
-        <div className="left">
+  const [loading, setLoading] = useState(false); // awaiting authentication
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-                <img src={logo} alt="Logo" style={{width:"20%"}} className="logo"/>
-   
-                <h1 className="sign_into"> Sign into</h1>
-                <h4 className="your_account"> {msg} </h4>
+    try {
+      setLoading(true);
+      const { user, session, error } = await supabase.auth.signIn({
+        email: formState.email,
+        password: formState.password,
+      });
 
-                <form>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" value={formState.email}
-                        onChange={updateState}
-                    ></input>
+      if (error) throw error;
+      alert("Successful!");
+    } catch (error) {
+      alert(error.error_description || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" value={formState.password}
-                        onChange={updateState}
-                    ></input>
+  return (
+    <div className="left">
+      <img src={logo} alt="Logo" style={{ width: "20%" }} className="logo" />
 
-                    <div style={{marginBottom: "1rem"}}>
-                        <input type="checkbox" id="remember-me" name="remember-me"></input>
-                        <label htmlFor="remember-me">Remember Me</label>
-                    </div>
+      <h1 className="sign_into"> Sign into</h1>
+      <h4 className="your_account"> {msg} </h4>
 
-                    <button type="button" onClick={handleSignIn}>Sign In</button>
-                </form>
-            </div>
-    )
+      <form onSubmit={handleLogin}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formState.email}
+          onChange={updateState}
+        ></input>
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formState.password}
+          onChange={updateState}
+        ></input>
+
+        <div style={{ marginBottom: "1rem" }}>
+          <input type="checkbox" id="remember-me" name="remember-me"></input>
+          <label htmlFor="remember-me">Remember Me</label>
+        </div>
+
+        <button type="button" onClick={handleLogin}>
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Left;
