afc7dd0a8ebbb5d8a454819f96664d1978b24cbd
Pei Cheng Yi <53963433+SeekSaveServe@users.noreply.github.com>
2022-05-24 21:58:05 +0800

    added todo

--- src/features/sign up/PrivacySettings.js
+++ src/features/sign up/PrivacySettings.js
@@ -116,7 +116,10 @@ function PrivacySettings() {
         }
       }
     }
-    // Add a check for error or smth
+    // TODO: Add a check for error or smth
+    // TODO: Unique communities categories
+    // TODO: Get the unique communities/skills/etc from db
+    // TODO: Loading
     // alert("Success!");
     // navigate("/feed", { replace: true });
   }
