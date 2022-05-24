f1e333e8e01ad0fd54f638fe274e113b4a4cbdd1
Huzaifa Raghav <windowpane1712@gmail.com>
2022-05-24 10:33:53 +0800

    Add structure to PrivacySettings

--- src/features/sign up/PrivacySettings.js
+++ src/features/sign up/PrivacySettings.js
@@ -1,5 +1,39 @@
+import { Paper, Typography, Checkbox, FormControlLabel, 
+    FormControl, FormLabel, FormGroup } from "@mui/material";
+import logo from "../../Assets/Easylink Logo Full.png";
+
+// rest is props for Checkbox, not FCL
+function CheckboxWithLabel({ label, ...rest }) {
+    return (
+        <FormControlLabel 
+          label={label} 
+          control={<Checkbox {...rest} />}>
+        </FormControlLabel>
+    )
+}
+
 function PrivacySettings() {
-    
+    return (
+        <>
+        <img src={logo} alt="Logo" style={{width: "250px"}} />
+        <Paper>
+            <Typography variant="h5">How would you like to be contacted by linked users?</Typography>
+            
+            <FormControl>
+                <FormLabel>
+                    <Typography variant="h5">Contact details visibility:</Typography>
+                </FormLabel>
+
+                <FormGroup row>
+                    <CheckboxWithLabel label="Telegram" />
+                    <CheckboxWithLabel label="Email" />
+                </FormGroup>
+            </FormControl>
+
+            
+        </Paper>
+        </>
+    )
 }
 
 export default PrivacySettings;
