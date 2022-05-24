4970135e0f0d6902ed54d84dc33af22a606a5f84
Pei Cheng Yi <53963433+SeekSaveServe@users.noreply.github.com>
2022-05-24 15:42:28 +0800

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
 import SignIn from "./features/sign_in/SignIn";
-import RegisterationTags from "./features/Registration Tags/RegistrationTags";
 function App() {
   return (
     <div>
-      <RegisterationTags />
+      <SignIn />
     </div>
   );
 }
