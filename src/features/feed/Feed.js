9e7474d6c2f189a0159fc44870464d1e68f79869
Huzaifa Raghav <windowpane1712@gmail.com>
2022-05-25 20:57:52 +0800

    Comments

--- src/features/feed/Feed.js
+++ src/features/feed/Feed.js
@@ -1,9 +1,16 @@
+import { useRef } from "react";
+import BasicButton from "../../components/BasicButton";
+import { supabase } from '../../supabaseClient';
+
 
 function Feed() {
+    const signOut = async() => {
+        await supabase.auth.signOut();
+    }
     return (
         <div>
-            This is just a test Feed
-            
+            <div>This is just a test Feed</div>
+            <BasicButton onClick={signOut} bg="primary" sx={{width: "300px"}}>Sign Out</BasicButton>
         </div>
     )
 }
