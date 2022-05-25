ac96dd76a1cbabc0aacb89bcf10f2021692bc5b6
Huzaifa Raghav <windowpane1712@gmail.com>
2022-05-25 19:46:01 +0800

    Add session effect to App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import logo from "./logo.svg";
 import SignIn from "./features/sign_in/SignIn";
 import RegistrationTags from "./features/Registration_Tags/RegistrationTags";
+import { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
+
 function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+	  setSession(supabase.auth.session());
+
+	  supabase.auth.onAuthStateChange((_event, session) => {
+		  setSession(session);
+	  });
+  }, []);
+
   return (
     <div>
       <SignIn />
