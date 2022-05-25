0ade99c10f751d851a273855b1d57d65f69d9da6
Huzaifa Raghav <windowpane1712@gmail.com>
2022-05-25 14:09:32 +0800

    Create basic alert

---
+++ src/components/Alert/Alert.js
@@ -0,0 +1,32 @@
+import { Alert as MuiAlert, Snackbar } from "@mui/material";
+import { forwardRef, useState } from "react";
+
+const Alert = forwardRef(function Alert(props,ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+function BasicAlert({ open, setOpen }) {
+    const [open, setOpen] = useState(false);
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') return;
+        setOpen(false);
+    }
+
+    return (
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+            This is a success message!
+            </Alert>
+        </Snackbar>
+    )
+}
+
+
+
+// function Alert() {
+//     return (
+//         <Snackbar open>
+            
+//         </Snackbar>
+//     )
+// }
