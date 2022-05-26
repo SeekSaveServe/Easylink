608c69c05843102944034b47622a69b17238bf67
Huzaifa Raghav <windowpane1712@gmail.com>
2022-05-26 16:36:58 +0800

    Add split-scroll recommendations and posts for feed

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,10 +1,39 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Card, CardHeader, CardContent, Stack } from "@mui/material";
 import styles from './Posts.module.css';
+import scroll from '../components/scroll/Scroll.module.css';
 
-function PostsList() {
+
+function PostCard() {
     return (
-        <Box className={styles.posts}>
+        <Card variant="outlined">
+            <CardHeader title="USDevs" subheader="4 days ago"/>
+
+            <CardContent>
+                <Typography variant="h4">Workshop</Typography>
+                <Typography variant="body2">
+                Good day to all! This is to announce our workshop happening on 14th May. Please come if you want to learn about Node.js!
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
+function PostsList({ className }) {
+    function showPosts(n) {
+        let arr = [];
+        for (let i = 0; i < n; i++) {
+            arr.push(<PostCard/>);
+        }
+        return arr;
+    }
+
+    return (
+        <Box className={`${scroll.scroll_parent}`}>
             <Typography variant="h4" color="var(--secondary)">Posts</Typography>
+            {/* <Stack spacing={4} sx={{padding: "1rem"}}> */}
+            <Box className={scroll.scroll_child}>
+                { showPosts(10) }
+            </Box>
+            {/* </Stack> */}
         </Box>
     )
 }
--- src/features/recommendations/RecommendationsList.js
+++ src/features/recommendations/RecommendationsList.js
@@ -1,10 +1,46 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Card, CardContent, CardHeader, Stack } from "@mui/material";
 import styles from './Recommendations.module.css';
+import scroll from '../components/scroll/Scroll.module.css';
 
-function RecommendationsList() {
+function RecommendationCard() {
     return (
-        <Box className={styles.recc}>
+        <Card variant="outlined">
+            <CardHeader title="USDevs" subheader="4 days ago"/>
+
+            <CardContent>
+                <Typography variant="h4">Make laundry chill again</Typography>
+                <Typography variant="body2">
+                Hi! We are Project Laundrobot, a sub-project under USDevs working on a hardware-based laundry notification system. We are looking for  
+                Developers who knows Python, Raspberry Pi
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
+function RecommendationsList({ className }) {
+    function showRecommendations(n) {
+        let arr = [];
+        for (let i = 0; i < n; i++) {
+            arr.push(<RecommendationCard/>);
+        }
+        return arr;
+    }
+
+    return (
+        // <Box className={styles.recc}>
+        //     <Typography variant="h4" color="var(--primary)">Recommendations</Typography>
+        //     {showRecommendations(2)}
+        // </Box>
+        // className={`${className} ${styles.recc_box}`}
+        <Box className={scroll.scroll_parent}>
             <Typography variant="h4" color="var(--primary)">Recommendations</Typography>
+
+            {/* <Box className={scroll.scroll_child}> */}
+                <Box className={scroll.scroll_child}>
+                    {showRecommendations(10)}
+                </Box>
+            {/* </Box> */}
         </Box>
     )
 
