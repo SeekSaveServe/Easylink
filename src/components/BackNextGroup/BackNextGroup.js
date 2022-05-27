e00c36efdab54889415f8dac9cf0862dec057e8a
Huzaifa Raghav <windowpane1712@gmail.com>
2022-05-27 14:47:40 +0800

    Set BackNextGroup to take click handlers

--- src/components/BackNextGroup/BackNextGroup.js
+++ src/components/BackNextGroup/BackNextGroup.js
@@ -3,19 +3,19 @@ import Stack from "@mui/material/Stack";
 import { Link } from "react-router-dom";
 import BasicButton from "../BasicButton";
 
-function BackNextGroup({ backRoute, nextRoute }) {
+function BackNextGroup({ backOnClick, nextOnClick }) {
     
     return (
         <Stack direction="row" spacing={3}>
-          <Link to={SettingsBackupRestoreOutlined} style={{ textDecoration: "none", marginLeft: "1.5%", flexGrow: 1 }}>
+          <div style={{ textDecoration: "none", marginLeft: "1.5%", flexGrow: 1 }}>
               <BasicButton bg="primary" onClick={backOnClick}>Back</BasicButton>
-          </Link>
+          </div>
 
-          <Link to={nextRoute} style={{ textDecoration: "none", marginRight: "1.5%", flexGrow: 1 }}>
+          <div style={{ textDecoration: "none", marginRight: "1.5%", flexGrow: 1 }}>
             <BasicButton bg="secondary" onClick={nextOnClick}>
               Next
             </BasicButton>
-          </Link>
+          </div>
         </Stack>
     )
 }
