ccf7487c7fa9508669173cd548219561275b12c8
Huzaifa Raghav <windowpane1712@gmail.com>
2022-05-30 21:32:52 +0800

    Add Scrollable component

---
+++ src/components/Scrollable/Scrollable.jsx
@@ -0,0 +1,18 @@
+// nest children inside child div. apply styles to outer Box (sx)
+import { Box } from '@mui/system';
+import scroll from '../../features/components/scroll/Scroll.module.css';
+
+function Scrollable({sx, children}) {
+    return (
+        <Box sx={sx} className={scroll.scroll_parent}>
+            <Box className={scroll.scroll_child}>
+                {children}
+                <div style={{visibility: "hidden", height: "10vh"}}>
+                </div>
+            </Box>
+        </Box>
+
+    )
+}
+
+export default Scrollable;
