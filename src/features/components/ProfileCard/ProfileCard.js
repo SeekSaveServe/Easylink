6c1807f81f130a08539be2473cd8177af9067f39
Huzaifa Raghav <windowpane1712@gmail.com>
2022-05-30 20:35:09 +0800

    Add words to tags for ProfileCard

--- src/features/components/ProfileCard/ProfileCard.js
+++ src/features/components/ProfileCard/ProfileCard.js
@@ -21,11 +21,28 @@ function CardButton({ children, sx ,...rest}) {
         <BasicButton sx={{...sx, fontSize: "0.1rem", padding: "0.2rem 0.8rem", display: "inline", width: "auto"}} {...rest}>{children}</BasicButton>
     )
 }
+
+function Tag({ children, color,...rest}) {
+    return (
+        <Typography sx={{backgroundColor: color}} className={styles.tag} {...rest}>
+            { children }
+        </Typography>
+    )
+}
+
+const colors = ["#FFE977", "#77FFCE", "#77EFFF"];
+
 function ProfileCard(props) {
     const user = useSelector(getUser);
     let { isProject, info } = props;
     isProject = isProject ?? false;
     info = info ?? defaultInfo;
+
+    const wordsToTags = (tagStrings) => {
+        return tagStrings.map((str, idx) => {
+            return <Tag key={idx} color={colors[idx % colors.length]}>{str}</Tag>
+        });
+    }
 
     return (
         <Card className={styles.card}>
@@ -38,21 +55,20 @@ function ProfileCard(props) {
                     <CardContent sx={{mt:0}}>
                         <Typography variant="h5" sx={{fontSize: "1.4rem"}} gutterBottom> {info.title}</Typography>
                         <Typography variant="body1" color="text.secondary" sx={{fontSize:"1rem"}}> {info.bio} {" "}
-                        I am interested in {info.tags[1].join(", ")}. My skills are {info.tags[0].join(", ")}.</Typography>
+                        I am interested in {wordsToTags(info.tags[1])}. My skills are {wordsToTags(info.tags[0])}.</Typography>
 
 
                         <CardActions>
                             <Stack direction="row" spacing={2} sx={{ mt: 1, width: "100%"}}>
-                                <Typography variant="subtitle1" className={styles.tag} sx={{backgroundColor: "var(--tag-grey)", display: "inline-block"}}>{isProject ? "Project" : "User"}</Typography>
-                                { isProject ? <Typography variant="body" sx={{alignSelf: "center"}}> {info.dateRange} </Typography> : <></>}
-                                { isProject ? <CardButton bg="secondary">Follow</CardButton> : <></> }
+                                <Typography 
+                                    variant="subtitle1" 
+                                    className={styles.tag} 
+                                    sx={{backgroundColor: "var(--tag-grey)"}}>{isProject ? "Project" : "User"}</Typography>
+                                    { isProject ? <Typography variant="body" sx={{alignSelf: "center"}}> {info.dateRange} </Typography> : <></>}
+                                    { isProject ? <CardButton bg="secondary">Follow</CardButton> : <></> }
 
                                 <CardButton bg="primary">Link</CardButton>
-                                <CardButton bg="red" sx={{}}>Not for me</CardButton>
-{/* 
-                                <Stack direction="row" sx={{justifySelf: "end"}}>
-                                    
-                                </Stack> */}
+                                <CardButton bg="red">Not for me</CardButton>
                             </Stack>
                         </CardActions>
                     </CardContent>
