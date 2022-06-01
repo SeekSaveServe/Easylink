e22ccd31b624c1146fd6306e36cdab9f532d32e7
Huzaifa Raghav <windowpane1712@gmail.com>
2022-06-01 15:31:12 +0800

    Add basic TreeView to Projects

--- src/features/Projects/Projects.js
+++ src/features/Projects/Projects.js
@@ -2,22 +2,29 @@ import BasicButton from "../../components/BasicButton";
 import { supabase } from "../../supabaseClient";
 import { useSelector } from "react-redux";
 import { Box, Typography, Stack, Paper, Divider } from "@mui/material";
+import { TreeView, TreeItem } from "@mui/lab";
 import BasicNavBar from "../../components/BasicNavBar/BasicNavBar";
+import { ArrowRight, ArrowDropDown } from "@mui/icons-material";
+import styles from './Projects.module.css';
+import { Container } from "@mui/system";
 
 function Projects() {
   return (
     <>
       <BasicNavBar />
-      <h1> Work in Progress</h1>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/cbuZfY2S2UQ"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+      <Container className={styles.container} maxWidth={"md"}>
+        <Paper elevation={3} className={styles.paper}>
+          <TreeView defaultCollapseIcon={<ArrowDropDown />} defaultExpandIcon={<ArrowRight/>} sx={{fontSize:"1000px"}}>
+            <TreeItem label="USDevs" nodeId="1" sx={{fontSize: "10rem"}}>
+              <TreeItem label="Laundrobot" nodeId="2"></TreeItem>
+              <TreeItem label="Cinnabot" nodeId="3"></TreeItem>
+              <TreeItem label="USC Website" nodeId="4">
+                <TreeItem label="Booking System" nodeId="5"></TreeItem>
+              </TreeItem>
+            </TreeItem>
+          </TreeView>
+        </Paper>
+      </Container>
     </>
   );
 }
