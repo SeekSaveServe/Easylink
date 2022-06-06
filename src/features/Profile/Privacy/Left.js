947b48bc4c4f7fb7cd175f716c56c2acf4ee53a3
Pei Cheng Yi <53963433+SeekSaveServe@users.noreply.github.com>
2022-06-06 19:40:44 +0800

    added privacy settings (left)

--- src/features/Profile/Privacy/Left.js
+++ src/features/Profile/Privacy/Left.js
@@ -1,12 +1,79 @@
-import { useEffect } from "react";
+import {
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import useBasicAlert from "../../../components/Alert";
 
-export default function Left() {
-  const dispatch = useDispatch();
+export default function Left({ contact, setContact }) {
   const { BasicAlert, showAlert } = useBasicAlert("error");
-  const user = useSelector((state) => state.user);
 
-  const [teleVisibility, setTeleVisibility] = useEffect(false);
-  const [emailVisibility, setEmailVisibility] = useEffect(false);
+  function RadioWithLabel({ value, label, ...rest }) {
+    return (
+      <FormControlLabel
+        label={label}
+        value={value}
+        control={<Radio {...rest} />}
+      />
+    );
+  }
+
+  // update field with key = name attribute, to value = value attribute
+  const radioChange = (evt) => {
+    setContact({
+      ...contact,
+      [evt.target.name]: evt.target.value,
+    });
+  };
+
+  return (
+    <>
+      <Box>
+        <FormLabel>
+          <Typography variant="h5" color="black">
+            Telegram visibility:
+          </Typography>
+        </FormLabel>
+
+        <RadioGroup
+          row
+          value={contact.telegram_visibility}
+          onChange={radioChange}
+          name="telegram_visibility"
+        >
+          <RadioWithLabel value="afterlink" label="Only after linking" />
+          <RadioWithLabel value="everyone" label="Everyone" />
+        </RadioGroup>
+      </Box>
+
+      <Box
+        mt={2}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <FormLabel>
+          <Typography variant="h5" color="black">
+            Email visibility:
+          </Typography>
+        </FormLabel>
+
+        <RadioGroup row value={contact.email_visibility} onChange={radioChange}>
+          <RadioWithLabel value="afterlink" label="Only after linking" />
+          <RadioWithLabel
+            value="everyone"
+            label="Everyone"
+            name="email_visibility"
+          />
+        </RadioGroup>
+      </Box>
+    </>
+  );
 }
