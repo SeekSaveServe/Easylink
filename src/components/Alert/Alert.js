e15c94816a734878df93d4a65d5d595b345e53a4
Huzaifa Raghav <windowpane1712@gmail.com>
2022-06-08 14:27:13 +0800

    Change alert duration to 5s

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -14,7 +14,7 @@ export function BasicAlert({ message, severity, open, setOpen }) {
     }
 
     return (
-        <Snackbar open={open} autoHideDuration={2000} onClose={handleClose} 
+        <Snackbar open={open} autoHideDuration={5000} onClose={handleClose} 
             anchorOrigin={{vertical:'top', horizontal: 'center'}}
         >
             <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
