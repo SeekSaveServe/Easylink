b016cfe4e575c263c8b8df9288ede9910b5bb323
Huzaifa Raghav <windowpane1712@gmail.com>
2022-06-09 20:33:58 +0800

    Pull posts from DB

--- src/features/Projects/Posts.jsx
+++ src/features/Projects/Posts.jsx
@@ -1,15 +1,16 @@
-import { Button, Container, Paper } from "@mui/material";
+import { Button, Container, Paper, Typography } from "@mui/material";
 import BasicNavBar from "../../components/BasicNavBar/BasicNavBar";
 import NavButtonGroup from "./NavButtonGroup";
 import styles from './Projects.module.css';
 import PostCard from "./PostCard";
 import Scrollable from "../../components/Scrollable";
-import { Center } from "@chakra-ui/react";
+import { Center, CircularProgress } from "@chakra-ui/react";
 import { AddCircleOutlined } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { supabase } from "../../supabaseClient";
 import { useAlert } from "../../components/Alert/AlertContext";
+import { useSelector } from "react-redux";
 
 // For the Posts tab under Projects. Show posts and polls made by the switched project
 export const fakePosts = [
@@ -64,32 +65,52 @@ export const fakePosts = [
 
 // Posts / polls that are made by the switched project (owned)
 function Posts() {
-    const showPosts = (n) => {
-        let posts = [];
+    // const showPosts = (n) => {
+    //     let posts = [];
         
-        for (let i = 0; i < n; i++) {
-            posts.push( <PostCard sx={{width: "90%", ml:1, mt:1}} data={fakePosts[i % fakePosts.length]}/>)
-        }
+    //     for (let i = 0; i < n; i++) {
+    //         posts.push( <PostCard sx={{width: "90%", ml:1, mt:1}} data={fakePosts[i % fakePosts.length]}/>)
+    //     }
 
-        return posts;
-    }
+    //     return posts;
+    // }
     
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const pid = useSelector(state => state.user.pid); // if I'm seeing Posts, I should be in Project mode -> pid exists
     const showAlert = useAlert();
 
     async function getPosts() {
         try {
-        const { data, error } = await supabase
-            .from('posts')
-            .select('*');
+            setLoading(true);
+            const { data, error } = await supabase
+                .from('posts')
+                .select('*')
+                .match({ pid: pid})
+            
+            if (error) throw error;
+            setPosts(data);
 
         } catch (error) {
             showAlert(error.error_description || error.message, "error");
+        } finally {
+            setLoading(false);
         }
     }
 
-    useEffect(() => {
+    const postsDisplay = () => {
+        if (loading) {
+            return <CircularProgress />;
+        }
 
+        return posts.length == 0 ? <Typography variant="h6" color="gray" sx={{mt:1, fontWeight:"normal"}}>Nothing to show</Typography> : 
+            posts.map((post, idx) => {
+            return <PostCard sx={{width: "90%", ml:1, mt:1}} data={post} key={idx}/>
+        })
+    }
+
+    useEffect(() => {
+        getPosts();
     }, [])
 
     return (
@@ -107,7 +128,7 @@ function Posts() {
 
         <Scrollable height="25vh">
             <div style={{display: "flex", flexDirection: "column", alignItems: "center", gap: "1rem",}}>
-                { showPosts(6) }
+                { postsDisplay() }
             </div>
         </Scrollable>
         </>
