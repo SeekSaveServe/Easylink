5f11e70e4526a1eb23076fb2c86aec5ec88f4ced
Pei Cheng Yi <53963433+SeekSaveServe@users.noreply.github.com>
2022-06-10 17:39:36 +0800

    removed console log

---
+++ backend/mc/ordersys/app/models/remote.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Remote(models.Model):                 # the class for the remote data
+    _DATABASE = 'remote'
+    
+    name = models.CharField(max_length=20)
+    data = models.JSONField()
+    
