2cf81f15aefef9de57ae50da071d82aa3cecca20
Pei Cheng Yi <53963433+SeekSaveServe@users.noreply.github.com>
2022-06-21 19:36:39 +0800

    vercel attempt 2

--- backend/mc/ordersys/ordersys/settings.py
+++ backend/mc/ordersys/ordersys/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-p$n1ubzvq5bj@!m-xqu*pn+or^)%(@scn4(w$ho8f$n-%@51jo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['murmuring-basin-78610.herokuapp.com', '.vercel.app']
+ALLOWED_HOSTS = ['murmuring-basin-78610.herokuapp.com', '.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -141,7 +141,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 STATIC_URL = 'static/'
 
