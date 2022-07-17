0484a9a66db8165a5aa582e55553549f017a0419
Huzaifa Raghav <windowpane1712@gmail.com>
2022-07-18 00:07:18 +0800

    Add delete project confirmation dialog

---
+++ src/components/AlertDialog/AlertDialog.jsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { LoadingButton } from '@mui/lab';
+import { useState } from 'react';
+import { Stack } from '@mui/material';
+import { Center } from '@chakra-ui/react';
+// title, description, disagreeText, agreeText: String
+    // disagree/agreeText for buttons
+// agreeAction: function to run if user agrees
+export function AlertDialog({ title, description, disagreeText, agreeText, agreeAction, open, setOpen }) {
+
+  const [loading, setLoading] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleAgree = async() => {
+    setLoading(true);
+    await agreeAction();
+    setLoading(false);
+    handleClose();
+
+  }
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        sx={{
+            position: "absolute",
+            left:"0%",
+            top:"-60%"
+        }}
+      >
+        <DialogTitle id="alert-dialog-title">
+          {title}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {description}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Center>
+          <Stack direction="row" spacing={3}>
+            { loading ? <></> : <Button onClick={handleClose}>{disagreeText}</Button> }
+            <LoadingButton loading={loading} onClick={handleAgree} color="error"autoFocus>
+                {agreeText}
+            </LoadingButton>
+          </Stack>
+          </Center>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default function useAlertDialog() {
+    const [open, setOpen] = React.useState(false);
+    const openDialog = () => setOpen(true);
+
+    const CurriedAlertDialog = (props) => <AlertDialog open={open} setOpen={setOpen} {...props}/>
+
+    return {
+        openDialog,
+        AlertDialog: CurriedAlertDialog
+    }
+}
